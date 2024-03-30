69647e4b623cc88c6935999f0e0c0e3a2d06da1f
Screxy <dvbvladis@mail.ru>
2024-03-30 19:07:31 +0300

    fix: init sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS auth;
-USE auth;
-CREATE TABLE IF NOT EXISTS user (
-    id INT auto_increment,
-    primary key (id),
-    email varchar(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    access_token varchar(255)
-);
